da72ce260f6b627d4da9d253464d1291bc89d893
arifur985 <58823771+arifur985@users.noreply.github.com>
2024-04-28 02:22:37 +1000

    done

--- dbConnection.js
+++ dbConnection.js
@@ -1,22 +1,20 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://store:aVRaKV2e7JDhu86q@cluster0.fcgmcp8.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, {
     serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
     }
 });
 
-async function connect() {
-    try {
+// Create a function to ensure the client is connected
+async function getClient() {
+    if (!client.isConnected()) {
         await client.connect();
-        console.log("Connected to MongoDB");
-        return client.db("your-database-name"); // Add your database name here
-    } catch (error) {
-        console.error("Failed to connect to MongoDB", error);
-        process.exit(1);
     }
+    return client;
 }
 
-module.exports = connect;
+module.exports = { getClient };
--- models/cats.js
+++ models/cats.js
@@ -1,14 +1,17 @@
-const connect = require('../dbConnection');
+const { getClient } = require('../dbConnection'); // Make sure this path is correct and the function is properly exported from dbConnection
+
+async function getCollection() {
+    const client = await getClient(); // This assumes getClient ensures a connected client
+    return client.db().collection('Cats');
+}
 
 async function insertCat(cat) {
-    const db = await connect();
-    const collection = db.collection('Cats');
+    const collection = await getCollection();
     return collection.insertOne(cat);
 }
 
 async function getAllCats() {
-    const db = await connect();
-    const collection = db.collection('Cats');
+    const collection = await getCollection();
     return collection.find().toArray();
 }
 
