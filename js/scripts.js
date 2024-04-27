a7ef99c097d9988e3888a6134703e87094e92c9b
arifur985 <58823771+arifur985@users.noreply.github.com>
2024-04-28 03:19:16 +1000

    done

--- js/scripts.js
+++ js/scripts.js
@@ -5,55 +5,73 @@ socket.on('number', (message) => {
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
-        '</div><div class="card-content">'+
-        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-        '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-        '</div></div></div>';
-        $("#card-section").append(itemToAppend)
+      let itemToAppend = `<div class="col s4 center-align">
+        <div class="card medium">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="${item.image}">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+            <p><a href="${item.linkUrl || '#'}">${item.link}</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+            <p class="card-text grey-text text-darken-4">${item.description}</p>
+          </div>
+        </div>
+      </div>`;
+      $('#card-section').append(itemToAppend);
     });
-}
-
-function submitForm() {
-    let formData = {};
-    formData.title = $('#title').val();
-    formData.path = $('#path').val();
-    formData.link = $('#link').val();
-    formData.description = $('#description').val();
+  };
+  
+  const clickMe = () => {
+    alert('Thanks for clicking me. Hope you have a nice day!')
+  }
+  
+  const submitForm = () => {
+    let formData = {
+        title: $('#title').val(),
+        image: $('#path').val(),  // Changed from $('#image').val() to $('#path').val()
+        link: $('#subTitle').val(),  // Assuming 'link' should use the subtitle/color
+        description: $('#description').val()
+    };
     
-    console.log("Form Data Submitted: ", formData);
-    postCat(formData);
-}
-
-function getCats() {
-    $.get('/api/cats', (response) => {
-        console.log(response.data);
-        if (response.data) {
-            addCards(response.data);
-        }
-    })
-}
-
-function postCat(cat) {
+    console.log('Form Data Submitted: ', formData);
+    postcats(formData);
+  };
+  
+  
+  const postcats = (project) => {
     $.ajax({
-        url: '/api/cats',
-        data: cat,
-        type: 'POST',
-        success: (result) => {
-            console.log(result.data);
-            location.reload();
-        }
+      url: '/api/cats',
+      data: JSON.stringify(project),
+      contentType: 'application/json',
+      method: 'POST',
+      success: (result) => {
+        alert('Cat posted');
+        location.reload();
+      },
+      error: (jqXHR, textStatus, errorThrown) => {
+        alert(`Error: ${textStatus}`);
+      }
     })
-}
-
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
-    getCats();
-    $('.modal').modal();
-    $('#formSubmit').click(()=>{
-        submitForm();
+  }
+  
+  const getproducts = () => {
+    $.get('/api/cats', (response) => {
+      if (response.statusCode === 200) {
+        addCards(response.data);
+      } else {
+        console.error(response);
+      }
+    }).fail((jqXHR, textStatus, errorThrown) => {
+      console.error(`Error fetching products: ${textStatus}`);
     });
-});
+  }
+  
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(submitForm);
+    $('.modal').modal();
+    getproducts();
+  });
